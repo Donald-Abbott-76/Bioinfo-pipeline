5eab3dad518f543099785e0243fe4a76dad5f96a
Donald abbott <donaldmajiwa76@gmail.com>
2026-03-30 23:21:09 +0200

    added lines of code for variant calling in pipeline.py

---
+++ src/pipeline.py
@@ -0,0 +1,17 @@
+#Variant calling
+
+print("Started analysis for variant calling.....")
+
+variants_found = 0
+
+for i in range(10):
+    print(f"Analyzing variant sequence {i}")
+    variants_found += 1
+
+print("compiling variant sequences")
+
+print("Variant Analysis completed")
+
+print("samples found:", variants_found)
+
+print("variant_name.txt" "\n" "variant.zipped")
